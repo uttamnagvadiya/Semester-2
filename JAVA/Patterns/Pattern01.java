1289b996bf668dcbec4ec562d0ae55879f35feb9
uttamnagvadiya <uttamnagvadiya192@gmail.com>
2022-03-26 20:54:02 +0530

    .

---
+++ JAVA/Patterns/Pattern01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Pattern01
+{
+    public static void main (String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = input.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+// Output is:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
