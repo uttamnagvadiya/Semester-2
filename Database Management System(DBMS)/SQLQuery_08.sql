9b19c95d4d94b01afef7f7de23a7aa89d05072e3
uttamnagvadiya <94751872+uttamnagvadiya@users.noreply.github.com>
2022-03-28 18:56:28 +0530

    Update SQLQuery_08.sql

--- Database Management System(DBMS)/SQLQuery_08.sql
+++ Database Management System(DBMS)/SQLQuery_08.sql
@@ -28,41 +28,50 @@ Insert into Employee Values(107, 'Neha', 'HR', 34000, '1990-Dec-25', 'Rajkot')
 
 
 Select max(Salary) as Max_Sal,
-	   min(Salary) as Min_Sal,
-	   sum(Salary) as Total_Sal,
-	   avg(Salary) as Avg_Sal
+       min(Salary) as Min_Sal,
+       sum(Salary) as Total_Sal,
+       avg(Salary) as Avg_Sal
 From Employee
+
 
 Select count(*) as Total_Record
 From Employee
+
 
 Select max(Salary) as Max_Sal_IT
 From Employee
 Where Department = 'IT'
 
+
 Select count(Distinct City) as Num_of_City
 From Employee
+
 
 Select City, count(*) as Total_Emp
 From Employee
 Group by City
+
 
 Select City, count(*) as Total_Emp
 From Employee
 Group by City
 Having count(*)>1
 
+
 Select Department, sum(Salary) as Total_Sal
 From Employee
 Group by Department
+
 
 Select avg(Salary) as Avg_Sal
 From Employee
 Group by Department
 
+
 Select min(Salary) as Min_Sal
 From Employee
 Where City = 'Ahmedabad'
+
 
 Select Department, sum(Salary) as Total_Sal
 From Employee
@@ -70,16 +79,20 @@ Where City = 'Rajkot'
 Group by Department
 Having sum(Salary)>5000
 
+
 Select count(*) as Total_Emp
 From Employee
 Where City = 'Rajkot'
 
+
 Select max(Salary) - min(Salary) as Sal_Difference
 From Employee
+
 
 Select count(*) as Hired_Emp
 From Employee
 Where JoiningDate < '1991-Jan-01'
+
 
 Select Department, sum(Salary) as Total_Sal
 From Employee
@@ -87,35 +100,42 @@ Group by Department
 Having sum(Salary)>35000
 Order by sum(Salary)
 
+
 Select Department, count(*) as Dep_Emp
 From Employee
 Group by Department
 Having count(*)>2
 
+
 Select min(Salary) as Min_Sal
 From Employee
 Where City = 'Rajkot'
+
 
 Select City, count(*)
 From Employee 
 Group by City
 
+
 Select Department, min(Salary) as Min_Sal
 From Employee
 Group by Department
+
 
 Select sum(Salary) as Total_Sal
 From Employee
 Group by City
 
+
 Select Department, min(Salary) as Min_Sal, 
-				   max(Salary) as Max_Sal,
-				   sum(Salary) as Total_Sal
+		   max(Salary) as Max_Sal,
+		   sum(Salary) as Total_Sal
 From Employee
 Group by Department
 
+
 Select City, Department, min(Salary) as Min_Sal,
-						 max(Salary) as Max_Sal
+			 max(Salary) as Max_Sal
 From Employee
 Group by City, Department
 Having min(Salary) < 30000
