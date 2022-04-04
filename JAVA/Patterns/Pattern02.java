6c18e42b0ddd47a12897242e18d28314c1c055d6
uttamnagvadiya <94751872+uttamnagvadiya@users.noreply.github.com>
2022-04-04 07:00:12 +0530

    Update Pattern02.java

--- JAVA/Patterns/Pattern02.java
+++ JAVA/Patterns/Pattern02.java
@@ -18,9 +18,6 @@ public class Pattern02{
 
         for(int i=0; i<n; i++)                                     
         {
-            if(i==1){
-                continue;
-            }
             for(int j=i+1; j<n; j++)
             {
                 System.out.print(" ");
@@ -32,9 +29,6 @@ public class Pattern02{
             System.out.print("\n");
         }
         for(int i=n-1; i>0; i--){
-            if(i==2){
-                continue;
-            }
             for(int j=i; j<n; j++)
             {
                 System.out.print(" ");
