1318fd311d16507891b5e0320950b4b00be1da53
uttamnagvadiya <uttamnagvadiya192@gmail.com>
2022-04-18 22:21:59 +0530

    .

--- JAVA/006_Classes and Inheritance/My2Points.java
+++ JAVA/006_Classes and Inheritance/My2Points.java
@@ -41,33 +41,32 @@ class Mypoint{
         this.y=y;
     }
     double distance(Mypoint a){
-        double k;
-        k= Math.sqrt(Math.pow(this.x-a.x,2)+Math.pow(this.y-a.y,2));
-        return k;
+       double D = Math.sqrt(Math.pow(this.x-a.x,2)+Math.pow(this.y-a.y,2));
+        return D;
     }
 }
 
-class Setxy{
+class InputXY{
     double x,y;
     Scanner input = new Scanner(System.in);
 
     public double getX() {
         System.out.print("Enter x : ");
-        return x= input.nextDouble();
+        return x = input.nextDouble();
     }
     public double gety() {
         System.out.print("Enter y : ");
-        return y= input.nextDouble();
+        return y = input.nextDouble();
     }
 }
 
 public class My2Points {
     public static void main(String[] args) {
-        Setxy ab = new Setxy();
+        InputXY ab = new InputXY();
         Mypoint obj1 = new Mypoint();
 
         Mypoint obj2 = new Mypoint(ab.getX(), ab.gety());
-        System.out.printf("distance (%.2f,%.2f) to (%.1f,%.1f) : ",obj1.x,obj1.y,obj2.x,obj2.y);
+        System.out.printf("distance (%.1f,%.1f) to (%.1f,%.1f) : ",obj1.x,obj1.y,obj2.x,obj2.y);
         System.out.println(obj1.distance(obj2));
 
         Mypoint obj3 = new Mypoint(ab.getX(), ab.gety());
