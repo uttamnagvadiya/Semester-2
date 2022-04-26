2a975d2c59595fc4fed9edc208bbe6426d051fc2
uttamnagvadiya <94751872+uttamnagvadiya@users.noreply.github.com>
2022-04-26 09:00:46 +0530

    Add files via upload

---
+++ JAVA/VegetableDemo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+abstract class Vegetable {
+	String color;
+	abstract String tostring();
+}
+
+class Potato extends Vegetable{
+	public String tostring(){
+		color = "Skin";
+		return color;
+	}
+}
+class Brinjal extends Vegetable{
+	public String tostring(){
+		color = "Purple";
+		return color;
+	}
+}
+class Tomato extends Vegetable {
+	public String tostring(){
+		color = "Red";
+		return color;
+	}
+}
+
+public class VegetableDemo{
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Vegetable Name : ");
+		String veg = input.next();
+
+		Vegetable v=null;
+
+		switch(veg){
+			case "Potato":
+				v = new Potato();
+				break;
+			case "Brinjal":
+				v = new Tomato();
+				break;
+			case "Tomato":	
+				v = new Tomato();
+				break;
+		}
+		
+		System.out.println(veg + ":" + v.tostring());
+	}
+}
